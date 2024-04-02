7a3608f22eb3a75933a69d8cbe81c6ccf6d42936
Man Kin Choi <mankinchoi@Mans-MacBook-Pro.local>
2024-04-03 00:31:53 +0800

    create textPreprocessing.py

--- textPreprocessing.py
+++ textPreprocessing.py
@@ -4,21 +4,38 @@ import nltk
 from nltk.corpus import stopwords
 from stop_words import get_stop_words
 from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize, wordpunct_tokenize, sent_tokenize,WhitespaceTokenizer
+from nltk.tokenize.treebank import TreebankWordTokenizer, TreebankWordDetokenizer
 
 
 
 
 
 class textPreprocessor:
-    def toLower(self,text): #take input as a string of texts
+    def toLower(self,text): #take input as string of text
         return text.lower()
-    def tokenize(self,text):
-        pass
     
-    def removeStopwords(self, word_list): 
+    def tokenize(self,text,mode):
+        if mode == "ws": #whitespace
+            return text.split()
+        elif mode == "l": #line
+            return nltk.sent_tokenize(text)
+        elif mode == "w": #word
+            return nltk.word_tokenize(text)
+        elif mode == "p": #punctuation base
+            return nltk.wordpunct_tokenize(text)
+        else: #treebankword
+            tokenizer = TreebankWordTokenizer()
+            return tokenizer.tokenize(text)
+
+            
+
+        
+    
+    def removeStopwords(self, word_list): #take input as list of words
         nltk.download('stopwords')
-        stop_words = list(get_stop_words('en'))         #About 900 stopwords
-        nltk_words = list(stopwords.words('english')) #About 150 stopwords
+        stop_words = list(get_stop_words('en'))  
+        nltk_words = list(stopwords.words('english')) 
         stop_words.extend(nltk_words)
 
         output = [word for word in word_list if not word in stop_words]
@@ -26,10 +43,9 @@ class textPreprocessor:
         return output
 
 
-    def lemmatizate(self,word_list):
+    def lemmatizate(self,word_list): #take input as list of words
         nltk.download('wordnet')
         wnl = WordNetLemmatizer()
-        word_list = ["I","am","going","home"]
         output = [wnl.lemmatize(word) for word in word_list]
 
         return output
@@ -47,7 +63,7 @@ class textPreprocessor:
 
 
 def main():
-
+    pass
 
   
 
