1f6258d69bd67ba3627ae0f0e9fc3c0a324d9f44
Man Kin Choi <mankinchoi@Mans-MacBook-Pro.local>
2024-04-07 02:32:06 +0800

    Tested with scrapped_articles.csv

--- TextPreprocessing.py
+++ TextPreprocessing.py
@@ -1,17 +1,22 @@
 import pandas as pd
-import numpy as np
+import re
 import nltk
 from nltk.corpus import stopwords
 from stop_words import get_stop_words
 from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize, wordpunct_tokenize, sent_tokenize,WhitespaceTokenizer
-from nltk.tokenize.treebank import TreebankWordTokenizer, TreebankWordDetokenizer
+#from nltk.tokenize import word_tokenize, wordpunct_tokenize, sent_tokenize,WhitespaceTokenizer
+from nltk.tokenize.treebank import TreebankWordTokenizer
 from nltk.corpus import wordnet
 
 
 class TextPreprocessor:
     def toLower(self,text): #take input as string of text
         return text.lower()
+    
+    def removeSpecChar(self, text): #remove special char and num
+        pattern = r'[^a-zA-Z\s]'
+        cleaned_text = re.sub(pattern, "", text)
+        return cleaned_text
     
     def tokenize(self,text,mode):
         if mode == "ws": #whitespace
@@ -27,8 +32,7 @@ class TextPreprocessor:
             return tokenizer.tokenize(text)
 
     
-    def removeStopwords(self, word_list): #take input as list of words
-        nltk.download('stopwords')
+    def remvStopwords(self, word_list): #take input as list of words
         stop_words = list(get_stop_words('en'))  
         nltk_words = list(stopwords.words('english')) 
         stop_words.extend(nltk_words)
@@ -39,13 +43,12 @@ class TextPreprocessor:
 
 
     def lemmatizate(self,word_list): #take input as list of words
-        nltk.download('wordnet')
         wnl = WordNetLemmatizer()
         output = [wnl.lemmatize(word) for word in word_list]
 
         return output
     
-    def removeSynonyms(self, word_list): #take input as a list of words
+    def remvSynonyms(self, word_list): #take input as a list of words
         syns = []
         for i in range(len(word_list)):
             if word_list[i] in set(syns): #the word is a synonym of other words appeared before
@@ -57,19 +60,76 @@ class TextPreprocessor:
                     for lem in s.lemmas():
                         syns.append(lem.name())
         return  list(filter(lambda a: a != "", word_list))
-       
+    
+   
+    def tagPOS(self, word_list): ##take input as a list of words
+       return  nltk.pos_tag(word_list)
+           
            
 
 
 
 
 
-
-
 def main():
-    pass
+    #download essential lib
+    """
+    nltk.download('stopwords')
+    nltk.download('wordnet')
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
+    """
+    #test with scrapped_articles.csv
+    tp = TextPreprocessor()
+    articles_df = pd.read_csv("./SalesEQ/scrapped_articles.csv",index_col = 0)[:500] 
+    cleaned_data = {"newHeadline":[],"newText":[]}
+    
+    for i in range(len(articles_df)):  
+        #lowercasing
+        headline = tp.toLower(articles_df.loc[i,"Headline"])
+        text = tp.toLower(articles_df.loc[i,"Text"])
 
-  
+        #remove special characters
+        headline = tp.removeSpecChar(headline)
+        text = tp.removeSpecChar(text)
+
+
+        #tokenization
+        headline = tp.tokenize(headline, mode = "w")
+        text = tp.tokenize(text, mode = "w")
+
+        #remove stopwords
+        headline = tp.remvStopwords(headline)
+        text = tp.remvStopwords(text)
+
+        #lemmatize
+        headline = tp.lemmatizate(headline)
+        text = tp.lemmatizate(text)
+
+        #remove synonym
+        headline = tp.remvSynonyms(headline)
+        text = tp.remvSynonyms(text)
+
+
+
+        headline = " ".join(headline)
+        text = " ".join(text)
+        
+        cleaned_data["newHeadline"].append(headline)
+        cleaned_data["newText"].append(text)
+
+    articles_df["newHeadline"] = cleaned_data["newHeadline"]
+    articles_df["newText"] = cleaned_data["newText"]
+
+    #print(articles_df)
+    #articles_df.to_csv("./SalesEQ/result.csv",index = False)
+
+        
+   
+
+
+    
+
 
 
 if __name__ == '__main__':
